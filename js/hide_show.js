1a09d4b911169094ad8472f6186e0a346ab8d092
Rishabh Srivastava <srivastava829@gmail.com>
2020-06-15 04:53:13 +0530

    "Major Bug Fix v2.1"

---
+++ js/hide_show.js
@@ -0,0 +1,24 @@
+$('#btn1').on('click', function () {
+    $('#one').show('slow')
+    $('#two').hide('slow')
+    $('#three').hide('slow')
+    $('#four').hide('slow');
+});
+$('#btn2').on('click', function () {
+    $('#one').hide('slow')
+    $('#two').show('slow')
+    $('#three').hide('slow')
+    $('#four').hide('slow');
+});
+$('#btn3').on('click', function () {
+    $('#one').hide('slow')
+    $('#two').hide('slow')
+    $('#three').show('slow')
+    $('#four').hide('slow');
+});
+$('#btn4').on('click', function () {
+    $('#one').hide('slow')
+    $('#two').hide('slow')
+    $('#three').hide('slow')
+    $('#four').show('slow');
+});
